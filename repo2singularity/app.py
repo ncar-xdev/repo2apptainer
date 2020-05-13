1417059bf6844c27e1842efdd7bbebca69c17be5
vagrant <vagrant@vagrant.vm>
2020-05-13 03:02:38 +0000

    Add build_sif method

--- repo2singularity/app.py
+++ repo2singularity/app.py
@@ -10,5 +10,18 @@ class Repo2Singularity(Repo2Docker):
     version = '0.0.1'
     description = __doc__
 
+    def build_sif(self):
+        from spython.main import Client
+
+        self.sif_image = f'{self.output_image_spec}.sif'
+        docker_uri = f'docker-daemon://{self.output_image_spec}:latest'
+        image, builder = Client.build(docker_uri, image=self.sif_image, stream=True)
+        print('\nBuilding singularity container from the built docker image...')
+        print(image)
+
+        for line in builder:
+            print(line)
+
     def start(self):
         self.build()
+        self.build_sif()
