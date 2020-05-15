cb571cde9f720f2c3a98081336e2d00e66c8b6ab
Anderson Banihirwe <axbanihirwe@ualr.edu>
2020-05-14 20:09:27 -0600

    Override methods for starting/running container

--- repo2singularity/app.py
+++ repo2singularity/app.py
@@ -1,15 +1,14 @@
 import os
-import subprocess
 import sys
+import tempfile
 from pathlib import Path
 
-from pkg_resources import DistributionNotFound, get_distribution
 from repo2docker.app import Repo2Docker
+from repo2docker.utils import chdir
+from spython.main import Client
+from spython.utils import get_username, stream_command
 
-try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:
-    __version__ = '0.0.0'
+from . import __version__
 
 SINGULARITY_CACHEDIR = os.environ.get('SINGULARITY_CACHEDIR', '~/.singularity/cache')
 CACHEDIR_PATH = Path(SINGULARITY_CACHEDIR).expanduser()
@@ -18,6 +17,7 @@ if not CACHEDIR_PATH.exists():
 
 REPO2SINGULARITY_CACHEDIR = CACHEDIR_PATH / 'repo2singularity'
 REPO2SINGULARITY_CACHEDIR.mkdir(exist_ok=True, parents=True)
+TMPDIR = tempfile.gettempdir()
 
 
 class Repo2Singularity(Repo2Docker):
@@ -30,10 +30,6 @@ class Repo2Singularity(Repo2Docker):
     description = __doc__
 
     def build_sif(self):
-        from spython.main import Client
-
-        self.singularity_image_name = f'{self.output_image_spec}.sif'
-        self.sif_image = f'{REPO2SINGULARITY_CACHEDIR}/{self.singularity_image_name}'
         docker_uri = f'docker-daemon://{self.output_image_spec}:latest'
         image, builder = Client.build(docker_uri, image=self.sif_image, stream=True)
         print('\nBuilding singularity container from the built docker image...')
@@ -58,14 +54,79 @@ class Repo2Singularity(Repo2Docker):
             self.sif_image,
             f'library://andersy005/test/{self.output_image_spec}:latest',
         ]
-        run_command(cmd)
+        stream_command(cmd)
+
+    def create_container_sandbox(self):
+        """
+        Pre-convert the Singularity Image File (SIF) to a directory based format (sandbox)
+        """
+        image, builder = Client.build(
+            image=self.sandbox_name,
+            recipe=self.sif_image,
+            build_folder=TMPDIR,
+            sandbox=True,
+            sudo=False,
+            stream=True,
+            force=True,
+        )
+        print(image)
+        for line in builder:
+            try:
+                print(line)
+            except Exception:
+                sys.exit(0)
+
+    def start_container(self):
+        """
+        Start singularity container from built image
+
+        Returns running container
+        """
+
+        host_name = '127.0.0.1'
+
+        self.host_name = host_name
+        if not self.run_cmd:
+            port = str(self._get_free_port())
+            ports = {f'{port}/tcp': port}
+            user = get_username()
+            run_cmd = [
+                'jupyter',
+                'lab',
+                '--ip',
+                '0.0.0.0',
+                '--port',
+                port,
+                f'--NotebookApp.custom_display_url=http://{host_name}:{port}',
+                '--notebook-dir',
+                f'{self.container_sandbox_dir}/home/{user}',
+            ]
+        else:
+            run_cmd = self.run_cmd
+            if self.ports:
+                ports = self.ports
+            else:
+                ports = {}
+        self.ports = ports
+
+        with chdir(Path(self.container_sandbox_dir).parent):
+            executor = Client.execute(
+                image=self.sandbox_name, writable=True, command=run_cmd, stream=True, quiet=False
+            )
+            for line in executor:
+                try:
+                    print(line)
+                except Exception:
+                    sys.exit(0)
 
     def start(self):
         self.build()
+        self.singularity_image_name = f'{self.output_image_spec}.sif'
+        self.sif_image = f'{REPO2SINGULARITY_CACHEDIR}/{self.singularity_image_name}'
+        self.sandbox_name = f'sandbox-{self.output_image_spec}'
+        self.container_sandbox_dir = f'{TMPDIR}/{self.sandbox_name}'
         self.build_sif()
         if self.push:
             self.push_image()
-
-
-def run_command(command):
-    _ = subprocess.check_call(command, stdout=sys.stdout, stderr=sys.stderr)
+        self.create_container_sandbox()
+        self.start_container()
