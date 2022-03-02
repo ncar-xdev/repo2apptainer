13e7921cd4201b5f4b3f0ec673a7bddd0354f6ee
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-03-01 22:49:49 -0700

    Update requirements and remove old app

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """The setup script."""
 from setuptools import find_packages, setup
 
 with open('requirements.txt') as f:
-    INSTALL_REQUIREs = f.read().strip().split('\n')
+    INSTALL_REQUIRES = f.read().strip().split('\n')
 with open('README.md', encoding='utf8') as f:
     LONG_DESCRIPTION = f.read()
 CLASSIFIERS = [
@@ -14,9 +14,9 @@ CLASSIFIERS = [
     'Intended Audience :: Science/Research',
     'Programming Language :: Python',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
     'Topic :: Scientific/Engineering',
 ]
 
@@ -25,16 +25,22 @@ setup(
     description='Repo2singularity: Wrapper around repo2docker producing producing Jupyter enabled Singularity images.',
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
-    python_requires='>=3.6',
+    python_requires='>=3.8',
     maintainer='Anderson Banihirwe',
     classifiers=CLASSIFIERS,
-    url='https://github.com/andersy005/repo2singularity',
+    url='https://repo2singularity.readthedocs.io',
+    project_urls={
+        'Documentation': 'https://repo2singularity.readthedocs.io',
+        'Source': 'https://github.com/andersy005/repo2singularity',
+        'Tracker': 'https://github.com/andersy005/repo2singularity/issues',
+        'Discussions/Support': 'https://github.com/andersy005/repo2singularity/discussions',
+    },
     packages=find_packages(exclude=('tests',)),
     include_package_data=True,
-    install_requires=INSTALL_REQUIREs,
-    license='Apache 2.0',
+    install_requires=INSTALL_REQUIRES,
+    license='BSD-3-Clause',
     zip_safe=False,
-    entry_points={'console_scripts': ['repo2singularity = repo2singularity.core:main']},
+    entry_points={'console_scripts': []},
     keywords='reproducible science environments docker singularity',
     use_scm_version={'version_scheme': 'post-release', 'local_scheme': 'dirty-tag'},
 )
