808cbd5c7f9035685fa7707f11bc2f98c6df3961
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-03-04 13:22:07 -0700

    Use timestamp as fallaback

--- repo2apptainer/helpers.py
+++ repo2apptainer/helpers.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import subprocess
+import time
 
 import escapism
 
@@ -27,21 +28,24 @@ def resolve_ref(repo_url: str, ref: str) -> str:
     Copied from https://github.com/yuvipanda/repo2charliecloud/blob/
                44a508b632e801d3b1f0dd1360e38f73d80efc74/repo2charliecloud/__init__.py#L6
     """
-    stdout = subprocess.check_output(['git', 'ls-remote', repo_url]).decode()
-    # ls-remote output looks like this:
-    # <hash>\t<ref>\n
-    # <hash>\t<ref>\n
-    # Since our ref can be a tag (so refs/tags/<ref>) or branch
-    # (so refs/head/<ref>), we get all refs and check if either
-    # exists
-    all_refs = [line.split('\t') for line in stdout.strip().split('\n')]
-    for git_hash, ref in all_refs:
-        if ref in (f'refs/heads/{ref}', f'refs/heads/{ref}'):
-            return git_hash[:7]  # Return first 7 characters
+    try:
+        stdout = subprocess.check_output(['git', 'ls-remote', repo_url]).decode()
+        # ls-remote output looks like this:
+        # <hash>\t<ref>\n
+        # <hash>\t<ref>\n
+        # Since our ref can be a tag (so refs/tags/<ref>) or branch
+        # (so refs/head/<ref>), we get all refs and check if either
+        # exists
+        all_refs = [line.split('\t') for line in stdout.strip().split('\n')]
+        for git_hash, ref in all_refs:
+            if ref in (f'refs/heads/{ref}', f'refs/heads/{ref}'):
+                return git_hash[:7]  # Return first 7 characters
 
-    if stdout:
-        return stdout.split()[0][:7]
-    return ref[:7]
+        if stdout:
+            return stdout.split()[0][:7]
+        return ref[:7]
+    except Exception:  # Use timestamp as fallback
+        return str(int(time.time()))
 
 
 def generate_image_name(repo: str, resolved_ref: str) -> str:
