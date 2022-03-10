84b932978ac47d0e79efe82490464282dac94660
Anderson Banihirwe <axbanihirwe@ualr.edu>
2022-03-10 12:25:02 -0700

    Remove sylabs-token gpg

--- .github/scripts/decrypt_secret.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Decrypt the file
-mkdir $HOME/secrets
-# --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$SYLABS_TOKEN" \
---output $HOME/secrets/sylabs-token sylabs-token.gpg
